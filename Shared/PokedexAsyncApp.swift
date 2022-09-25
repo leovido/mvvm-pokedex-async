749acb186d5f2d57a54b3e5c44ae9b8d20e8e207
Christian Leovido <christian.leovido@gmail.com>
2022-09-25 15:33:41 +0100

    Initial Commit

---
+++ Shared/PokedexAsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexAsyncApp.swift
+//  Shared
+//
+//  Created by Amethyst Raven Sky Wyld on 25/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct PokedexAsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
